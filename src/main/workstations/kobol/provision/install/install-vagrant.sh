7bc8b8b22f733de255b27f59d7c6fe3c238e2ca6
sebastian <sebastian@sommerfeld.io>
2022-04-30 17:13:00 +0200

    refactor: updated vagrant installation

--- src/main/workstations/kobol/provision/install/install-vagrant.sh
+++ src/main/workstations/kobol/provision/install/install-vagrant.sh
@@ -14,14 +14,28 @@
 # The script does not accept any parameters.
 
 
+echo -e "$LOG_INFO Purge any previous vagrant installation"
+sudo apt-get purge -y vagrant
+sudo dpkg --remove vagrant
+
 echo -e "$LOG_INFO Install vagrant"
-sudo apt-get install -y vagrant
+#curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+#sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+#sudo apt-get update
+#sudo apt-get install -y vagrant
+
+#curl https://releases.hashicorp.com/vagrant/2.2.19/vagrant_2.2.19_i686.deb --output vagrant.deb
+curl https://releases.hashicorp.com/vagrant/2.2.19/vagrant_2.2.19_x86_64.deb --output vagrant.deb
+sudo dpkg -i vagrant.deb
+rm vagrant.deb
+
+echo -e "$LOG_INFO $(vagrant --version)"
 
 echo -e "$LOG_INFO Install vagrant plugins"
 vagrant plugin install vagrant-cachier
 vagrant plugin install vagrant-vbguest
 vagrant plugin install vagrant-docker-compose
 
-echo -e "$LOG_INFO Install packer"
-sudo apt-get install -y packer
-echo -e "$LOG_DONE Installed vagrant"
+#echo -e "$LOG_INFO Install packer"
+#sudo apt-get install -y packer
+#echo -e "$LOG_DONE Installed vagrant"
