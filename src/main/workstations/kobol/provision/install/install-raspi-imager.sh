5cbcefa498c1127d8d35e3e330f8e5034011c187
sebastian <sebastian@sommerfeld.io>
2022-05-01 20:13:29 +0200

    fix: repaired RasPi imager installation

--- src/main/workstations/kobol/provision/install/install-raspi-imager.sh
+++ src/main/workstations/kobol/provision/install/install-raspi-imager.sh
@@ -2,7 +2,7 @@
 # @file install-raspi-imager.sh
 # @brief Install Raspi Imager.
 #
-# @description The script installs Raspi Imager.
+# @description The script installs Raspberry Pi Imager.
 #
 # ==== Arguments
 #
@@ -10,7 +10,6 @@
 
 
 echo -e "$LOG_INFO Install Raspi Imager"
-curl -L https://downloads.raspberrypi.org/imager/imager_latest_amd64.deb --output raspi-imager.deb
-sudo apt-get install -y ./raspi-imager.deb
-rm raspi-imager.deb
+sudo apt-get update
+sudo apt-get install -y rpi-imager
 echo -e "$LOG_DONE Installed Raspi Imager"
