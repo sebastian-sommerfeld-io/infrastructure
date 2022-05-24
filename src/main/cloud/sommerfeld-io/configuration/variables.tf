62da3f81b16b40e750589979ce8eab0bdc6807c7
sebastian <sebastian@sommerfeld.io>
2022-05-24 16:24:31 +0200

    fix: antora build broken due to folder-names starting with an underscore

--- src/main/cloud/sommerfeld-io/configuration/variables.tf
+++ src/main/cloud/sommerfeld-io/configuration/variables.tf
@@ -1,4 +1,4 @@
 variable "do_token" {
-  description = "Access token for Digital Ocean API (read in `tf.sh` from local file that is not pushed to the remote git repo)"
+  description = "Access token for DigitalOcean API (read in `tf.sh` from local file that is not pushed to the remote git repo)"
   type        = string
 }
