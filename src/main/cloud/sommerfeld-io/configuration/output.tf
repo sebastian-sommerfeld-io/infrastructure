3784d033862741e282e48df4c27c9e925214d2c0
sebastian <sebastian@sommerfeld.io>
2022-05-25 22:35:28 +0200

    refactor: file layout

--- src/main/cloud/sommerfeld-io/configuration/output.tf
+++ src/main/cloud/sommerfeld-io/configuration/output.tf
@@ -1,8 +1,3 @@
-output "linode_account_email" {
-  value       = data.linode_account.linode_account.email
-  description = "The email address of the Linode account in use"
-}
-
 output "do_account_email" {
   value       = data.digitalocean_account.do_account.email
   description = "The email address of the DigitalOcean account in use"
@@ -16,4 +11,11 @@ output "do_docs_page_live_url" {
 output "do_docs_page_default_url" {
   value = data.digitalocean_app.docs-page-data.default_ingress
   description = "The <random-subdomain>.ondigitalocean.app domain"
+}
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+output "linode_account_email" {
+  value       = data.linode_account.linode_account.email
+  description = "The email address of the Linode account in use"
 }
