1d9c73af8c72672658405ea342a0d49614aedc6d
sebastian <sebastian@sommerfeld.io>
2022-05-31 21:50:47 +0200

    refactor: removed docker provider

--- src/main/cloud/sommerfeld-io/configuration/docs-cloud-sommerfeld-io.tf
+++ src/main/cloud/sommerfeld-io/configuration/docs-cloud-sommerfeld-io.tf
@@ -1,12 +1,3 @@
-data "docker_registry_image" "docs-image" {
-  name = "sommerfeldio/docs-website:stable"
-}
-
-resource "docker_image" "docs-image" {
-  name          = data.docker_registry_image.docs-image.name
-  pull_triggers = [data.docker_registry_image.docs-image.sha256_digest]
-}
-
 resource "digitalocean_app" "docs-page" {
   spec {
     domain {
--- src/main/cloud/sommerfeld-io/configuration/provider.tf
+++ src/main/cloud/sommerfeld-io/configuration/provider.tf
@@ -9,11 +9,6 @@ terraform {
       source  = "linode/linode"
       version = "1.27.1"
     }
-
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "2.16.0"
-    }
   }
 }
 
@@ -28,7 +23,3 @@ provider "linode" {
 }
 
 data "linode_account" "linode_account" {}
-
-provider "docker" {
-  host = "unix:///var/run/docker.sock"
-}
