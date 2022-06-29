2b1f912dfe5f8205862dea5524b599bfdea16966
sebastian <sebastian@sommerfeld.io>
2022-06-29 13:17:32 +0200

    refactor: removed directory-validator and switched to folderslint

--- lint.sh
+++ lint.sh
@@ -11,7 +11,7 @@
 
 echo -e "$LOG_INFO Download latest linter definitions"
 linterDefinitions=(
-  '.directoryvalidator.json'
+  '.folderslintrc'
   '.ls-lint.yml'
   '.yamllint.yml'
 )
@@ -25,8 +25,8 @@ echo -e "$LOG_INFO -------------------------------------------------------------
 
 echo -e "$LOG_INFO Run linter containers"
 
-echo -e "$LOG_INFO check mandatory files"
-docker run -it --rm --volume "$(pwd):$(pwd)" --workdir "$(pwd)" sommerfeldio/directory-validator:latest directory-validator .
+echo -e "$LOG_INFO Lint filesystem structure"
+docker run -it --rm --volume "$(pwd):$(pwd)" --workdir "$(pwd)" pegasus/folderslint:latest folderslint
 
 echo -e "$LOG_INFO yamllint"
 docker run -it --rm --volume "$(pwd):/data" --workdir "/data" cytopia/yamllint:latest .
