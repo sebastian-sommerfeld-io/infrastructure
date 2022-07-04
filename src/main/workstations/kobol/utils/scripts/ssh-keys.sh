f88d7651ac5eaa2bc41e12d6a1c47b92e61f2005
sebastian <sebastian@sommerfeld.io>
2022-07-04 22:18:22 +0200

    feat: updated ssh ke generator

--- src/main/workstations/kobol/utils/scripts/ssh-keys.sh
+++ src/main/workstations/kobol/utils/scripts/ssh-keys.sh
@@ -10,7 +10,10 @@
 # * *$1* (String): KEY_NAME
 
 KEY_NAME="$1"
-KEY_FILE="$HOME/.ssh/$KEY_NAME.key"
+KEY_FILE=""
+
+YES="yes"
+NO="no"
 
 
 echo -e "$LOG_INFO Create ssh key-pair"
@@ -23,6 +26,17 @@ then
   echo -e "$LOG_ERROR exit"
   exit 0
 fi
+
+
+echo -e "$LOG_INFO Add '.key' to filename?"
+echo -e "$LOG_INFO     yes = '$P$KEY_NAME.key$D' and '$P$KEY_NAME.key.pub$D'"
+echo -e "$LOG_INFO     no  = '$P$KEY_NAME$D' and '$P$KEY_NAME.pub$D'"
+select s in "$YES" "$NO"; do
+  case "$s" in
+    "$YES" ) KEY_FILE="$HOME/.ssh/$KEY_NAME.key"; break;;
+    "$NO" )  KEY_FILE="$HOME/.ssh/$KEY_NAME"; break;;
+  esac
+done
 
 
 (
