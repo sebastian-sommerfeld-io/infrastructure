3bc47b89c2e7a00d92327500a3d5183a18987478
sebastian <sebastian@sommerfeld.io>
2022-07-04 22:29:33 +0200

    refactor: confirm to linter message

--- src/main/workstations/kobol/provision/11-configure-wrappers.sh
+++ src/main/workstations/kobol/provision/11-configure-wrappers.sh
@@ -28,7 +28,7 @@ function set_executable() {
   chmod +x "/usr/bin/$2"
 }
 
-WRAPPERS_PATH="$HOME/work/repos/sebastian-sommerfeld-io/infrastructure/src/main/workstations/kobol/docker/wrappers"
+#WRAPPERS_PATH="$HOME/work/repos/sebastian-sommerfeld-io/infrastructure/src/main/workstations/kobol/docker/wrappers"
 #set_executable "$WRAPPERS_PATH/ansible.sh" ansible
 #set_executable "$WRAPPERS_PATH/ansible.sh" ansible-playbook
 echo -e "$LOG_DONE Symlink setup for Docker wrapper scripts"
