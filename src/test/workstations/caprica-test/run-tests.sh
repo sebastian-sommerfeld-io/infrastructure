a6e27c7aaa5dba93960566d3e7ec94f62baa5883
sebastian <sebastian@sommerfeld.io>
2022-07-04 22:19:27 +0200

    feat: SIO-169 test ansible setup in vagrantbox

--- src/test/workstations/caprica-test/run-tests.sh
+++ src/test/workstations/caprica-test/run-tests.sh
@@ -20,27 +20,42 @@ case $HOSTNAME in
   (*)         echo -e "$LOG_ERROR Script not running on expected machine!!! Run on 'kobol' only!!!" && exit;;
 esac
 
+echo -e "$LOG_INFO Validate Vagrantfile"
+vagrant validate
 
 echo -e "$LOG_INFO Run tests for$P caprica-test$D"
-echo && docker run --rm mwendler/figlet:latest "Test: Provisioning" && echo
+echo && docker run --rm mwendler/figlet:latest "Startup" && echo
 
 echo -e "$LOG_INFO Create and startup Vagrantbox"
 vagrant up
 
 echo && docker run --rm mwendler/figlet:latest "Validate" && echo
+# todo ... chef inspec -> validate same stuff as with real caprica machine
 
+echo && docker run --rm mwendler/figlet:latest "Test" && echo
 echo -e "$LOG_INFO Test Docker installation"
 echo -e "$P"
 vagrant ssh -c "docker run --rm hello-world:latest"
 echo -e "$D"
 
-# todo ... chef inspec -> validate same stuff as with real caprica machine
+echo -e "$LOG_INFO Test git: clone repository from github via SSH"
+echo -e "$P"
+vagrant ssh -c "(cd repos && git clone https://github.com/sebastian-sommerfeld-io/playgrounds.git)" # todo ... clone via ssh
+echo -e "$D"
+
+echo -e "$LOG_INFO Test Virtualbox and Vagrant"
+echo -e "$P"
+vagrant ssh -c "vagrant --version"
+vagrant ssh -c "vboxmanage --version"
+vagrant ssh -c "vboxmanage list systemproperties | grep folder"
+echo -e "$D"
 
 echo && docker run --rm mwendler/figlet:latest "Shutdown" && echo
 
-echo -e "$LOG_INFO Shutdown Vagrantbox"
-vagrant halt
-
-echo -e "$LOG_INFO Remove Vagrantbox and cleanup filesystem"
-vagrant destroy -f
-rm -rf .vagrant
+#echo -e "$LOG_INFO Shutdown Vagrantbox"
+#vagrant halt
+#
+#echo -e "$LOG_INFO Remove Vagrantbox and cleanup filesystem"
+#vagrant destroy -f
+#rm -rf .vagrant
+#rm -rf ./*.log
