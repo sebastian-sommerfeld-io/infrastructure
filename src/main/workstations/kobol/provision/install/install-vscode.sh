39859d3e4d25c16d305f7190aff1bbe2ac7225e3
sebastian <sebastian@sommerfeld.io>
2022-07-07 10:32:43 +0200

    feat: install vscode

---
+++ src/main/workstations/kobol/provision/install/install-vscode.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# @file install-vscode.sh
+# @brief Install Visual Studio Code.
+#
+# @description The script installs Visual Studio Code.
+#
+# ==== Arguments
+#
+# The script does not accept any parameters.
+
+
+echo -e "$LOG_INFO Install VSCode"
+sudo snap install --classic code
