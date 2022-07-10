d59129bf6351028ce7e0402fbc29060babfd6fc2
sebastian <sebastian@sommerfeld.io>
2022-07-10 21:05:24 +0200

    refactor: reverted change

--- src/main/workstations/kobol/provision/install/install-ansible.sh
+++ src/main/workstations/kobol/provision/install/install-ansible.sh
@@ -15,10 +15,3 @@ GLOBAL_CONFIG="/etc/ansible/ansible.cfg"
 echo -e "$LOG_INFO Install ansible"
 sudo apt-get update
 sudo apt-get install -y ansible
-
-# echo -e "$LOG_INFO Update $GLOBAL_CONFIG to allow becomming an unprivileged user in a playbook"
-# sed -i 's/.*pipelining.*/pipelining = True/' /etc/ansible/ansible.cfg
-# sed -i 's/.*allow_world_readable_tmpfiles.*/allow_world_readable_tmpfiles = True/' /etc/ansible/ansible.cfg
-
-# sed -i 's/.*pipelining.*/pipelining = True/' "$GLOBAL_CONFIG"
-# sed -i 's/.*allow_world_readable_tmpfiles.*/allow_world_readable_tmpfiles = True/' "$GLOBAL_CONFIG"
