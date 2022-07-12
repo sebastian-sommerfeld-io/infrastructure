cdc0727ebc57242d65d03d1f2ea4fcb3b38c9aac
sebastian <sebastian@sommerfeld.io>
2022-07-12 22:14:58 +0200

    feat: install VSCode extensions from script

--- src/main/workstations/kobol/provision/install/install-ssh-server.sh
+++ src/main/workstations/kobol/provision/install/install-ssh-server.sh
@@ -14,4 +14,4 @@ sudo apt-get -y update
 sudo apt-get install -y openssh-server
 sudo ufw allow ssh # if the firewall is enabled on your system open the ssh port
 sudo systemctl enable --now ssh
-echo -e "$LOG_INFO Installed and started SSH server"
+echo -e "$LOG_DONE Installed and started SSH server"
--- src/main/workstations/kobol/provision/install/install-vscode.sh
+++ src/main/workstations/kobol/provision/install/install-vscode.sh
@@ -11,3 +11,29 @@
 
 echo -e "$LOG_INFO Install VSCode"
 sudo snap install --classic code
+
+echo -e "$LOG_INFO Install extensions"
+extensions=(
+  'redhat.ansible'
+  'asciidoctor.asciidoctor-vscode'
+  'aaron-bond.better-comments'
+  'chef-software.chef'
+  'ms-azuretools.vscode-docker'
+  'hashicorp.terraform'
+  'ms-toolsai.jupyter'
+  'ms-toolsai.jupyter-keymap'
+  'ms-toolsai.jupyter-renderers'
+  'ms-python.vscode-pylance'
+  'ms-python.python'
+  'rebornix.ruby'
+  'timonwong.shellcheck'
+  'wingrunr21.vscode-ruby'
+  'redhat.vscode-yaml'
+)
+for extension in "${extensions[@]}"
+do
+  echo -e "$LOG_INFO Install extension -> $extension"
+  code --install-extension "$extension"
+done
+
+echo -e "$LOG_DONE Installed VSCode"
